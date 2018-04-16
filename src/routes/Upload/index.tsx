33252075e47e47e86779a9d89d11f059a41be287
yiiu <yu.gintoki@gmail.com>
2018-04-16 23:33:36 +0800

    i

--- src/routes/Upload/index.tsx
+++ src/routes/Upload/index.tsx
@@ -5,12 +5,44 @@ import {
 import styles from './styles.less'
 
 export default class Upload extends React.PureComponent {
+  uploadInput: HTMLInputElement
+  state = {
+    image: null
+  }
+
+  imgUrl = null
+
+  _upload = () => {
+    console.log(this.uploadInput.click())
+  }
+
+  _inputChange = (e: any) => {
+    console.log(e.target.files[0])
+    var reader = new FileReader();  
+    reader.readAsDataURL(e.target.files[0]);
+    reader.onload = function (e: any) {
+      console.log(this.result)
+    }
+  }
+
   render () {
     return (
       <div className={styles.layout}>
         <section className={styles.box}>
           <h2>上传图片到Soap</h2>
-          <Button type="primary" className={styles.upload_btn}>上传</Button>
+          <Button
+            type="primary"
+            className={styles.upload_btn}
+            onClick={this._upload}
+          >
+            上传
+          </Button>
+          <input
+            ref={(e: HTMLInputElement) => this.uploadInput = e}
+            id={styles.upload_input}
+            type="file"
+            onChange={this._inputChange}
+          />
         </section>
       </div>
     )
